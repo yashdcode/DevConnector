bec7aa3394c9a2a59016faaaced419bd6e4c3167
yashdcode <74835997+yashdcode@users.noreply.github.com>
2025-07-26 02:34:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // your backend API
-        changeOrigin: true,
-        secure: false, // if you're using https locally and have self-signed cert
-      },
+ proxy: {
+    "/api": {
+      target: "https://dev-connector-backend-bbpc.vercel.app/",
+      changeOrigin: true,
+      secure: false,
     },
+  },
   },
 });
