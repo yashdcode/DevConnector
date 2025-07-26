8464a6ad939075729d239d9d210373756a39b38c
Yash Duddalwar <yashduddalwar@gmail.com>
2025-07-26 14:06:38 +0530

    bug fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
- proxy: {
-    "/api": {
-      target: "https://dev-connector-backend-bbpc.vercel.app/",
-      changeOrigin: true,
-      secure: false,
+    proxy: {
+      "/api": {
+        target: "https://dev-connector-backend-bbpc.vercel.app/",
+        // target: "http://localhost:5000/",
+        changeOrigin: true,
+        secure: false,
+      },
     },
-  },
   },
 });
